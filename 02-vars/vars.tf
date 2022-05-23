7edb44737252b5f801e74664223d942048745492
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-23 08:24:19 +0530

    Update learning/learning-terraform/

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -6,3 +6,40 @@ output "sample" {
   value = var.sample
 }
 
+# String Data type
+variable "sample1" {
+  default = "Hello World"
+}
+
+# Number data type
+variable "sample2" {
+  default = 100
+}
+
+# Boolean Data type
+variable "sample3" {
+  default = true
+}
+
+
+variable "sample4" {
+  default = [
+    "Hello",
+    1000,
+    true,
+    "World"
+  ]
+}
+
+variable "sample5" {
+  default = {
+    string  = "Hello",
+    number  = 100,
+    boolean = true
+  }
+}
+
+output "value-in-list" {
+  value = var.sample4[3]
+}
+
