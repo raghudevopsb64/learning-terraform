c5b275245e5fbdd10cc0efd26c7a40eb453007c9
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-23 08:49:41 +0530

    Update learning/learning-terraform/

--- 03-resources/main.tf
+++ 03-resources/main.tf
@@ -2,3 +2,18 @@ resource "aws_instance" "sample" {
   instance_type = "t3.micro"
   ami           = "ami-0fecabf044da28381"
 }
+
+provider "aws" {
+  region = "us-east-1"
+  version
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.15.1"
+    }
+  }
+}
+
