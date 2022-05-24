65cd85811e4c4abbe3a540823583194747caa64a
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-24 08:44:49 +0530

    Update learning/learning-terraform/

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -1,15 +1,9 @@
-module "frontend" {
+module "common" {
+  count         = length(var.COMPONENTS)
   source        = "./common"
   INSTANCE_TYPE = "t3.micro"
   AMI           = "ami-0fecabf044da28381"
-  COMPONENT     = "frontend"
-  PORT          = 80
+  COMPONENT     = var.COMPONENTS[count.index]
+  PORT          = var.PORTS[count.index]
 }
 
-module "monogdb" {
-  source        = "./common"
-  INSTANCE_TYPE = "t3.micro"
-  AMI           = "ami-0fecabf044da28381"
-  COMPONENT     = "mongodb"
-  PORT          = 27017
-}
---
+++ roboshop/vars.tf
@@ -0,0 +1,8 @@
+variable "COMPONENTS" {
+  default = ["frontend", "catalogue", "mongodb"]
+}
+
+variable "PORTS" {
+  default = ["80", "8080", "27017"]
+}
+
