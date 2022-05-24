3c3b0458502553037ed22a69a89c22bbe397a0a9
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-24 08:38:16 +0530

    Update learning/learning-terraform/

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -5,3 +5,11 @@ module "frontend" {
   COMPONENT     = "frontend"
   PORT          = 80
 }
+
+module "monogdb" {
+  source        = "./common"
+  INSTANCE_TYPE = "t3.micro"
+  AMI           = "ami-0fecabf044da28381"
+  COMPONENT     = "mongodb"
+  PORT          = 27017
+}
