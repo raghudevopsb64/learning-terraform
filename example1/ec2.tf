83f9d88c4d46c78e0f0935386af72407557c518c
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-24 07:46:09 +0530

    Update learning/learning-terraform/

--- example1/ec2.tf
+++ example1/ec2.tf
@@ -3,3 +3,15 @@ resource "aws_instance" "sample" {
   ami           = "ami-0fecabf044da28381"
 }
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 4.14.0"
+    }
+  }
+}
