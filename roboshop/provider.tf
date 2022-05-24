6c36f674530b8ef16a519aa8710542725217e4d2
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-24 08:33:30 +0530

    Update learning/learning-terraform/

---
+++ roboshop/provider.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-b64"
+    key    = "roboshop/terraform.tfstate"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 4.14.0"
+    }
+  }
+}
+
