108f3d69cc0d6156e745979908516c9d8e4574f7
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-24 07:52:08 +0530

    Update learning/learning-terraform/

--- example1/ec2.tf
+++ example1/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "sample" {
-  instance_type = "t3.micro"
-  ami           = "ami-0fecabf044da28381"
+  instance_type          = "t3.micro"
+  ami                    = "ami-0fecabf044da28381"
+  vpc_security_group_ids = [aws_security_group.allow_ec2.id]
 }
 
 provider "aws" {
