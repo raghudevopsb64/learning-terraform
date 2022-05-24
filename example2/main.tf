d0a0c22a720654bbe5296e09f582382f0f40a5be
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-24 08:15:10 +0530

    Update learning/learning-terraform/

---
+++ example2/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-b64"
+    key    = "example1/terraform.tfstate"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 4.14.0"
+    }
+  }
+}
